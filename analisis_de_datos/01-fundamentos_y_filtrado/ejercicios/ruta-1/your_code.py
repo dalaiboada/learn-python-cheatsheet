27359aad028335b88fc9fbd85ec7c7e47d7426d2
dalaiboada <dalaiboadajob@gmail.com>
2026-08-27 09:02:42 -0400

    Respuestas: fundamentos y filtrado (ruta 1 y 2)

--- analisis_de_datos/01-fundamentos_y_filtrado/ejercicios/ruta-1/your_code.py
+++ analisis_de_datos/01-fundamentos_y_filtrado/ejercicios/ruta-1/your_code.py
@@ -4,13 +4,29 @@ import pandas as pd
 df = pd.read_csv ('GoogleApps.csv')
 
 # ¿Cuál es el nombre de la primera aplicación en el conjunto de datos?
+# * Res: Photo Editor & Candy Camera & Grid & ScrapBook
+print(df.head(3))
+
 
 # ¿A qué categoría pertenece la última aplicación del conjunto de datos?
+# * Res: LIFESTYLE
+print(df.tail(3))
 
 # ¿Cuántas columnas hay en el conjunto de datos?
 # ¿Qué tipo de datos se almacenan en cada una de las columnas?
+""" 
+Reseña (Reviews)
+Content Rating(Clasificación de Contenido)
+Rating (Clasificación) 
+"""
+print(df.info())
 
 
 # Especifique la media aritmética y la mediana del tamaño de la aplicación (Tamaño)
-# ¿Cuánto cuesta la aplicación más cara?
-# * Especifique la media aritmética y la mediana del número de instalaciones de aplicaciones (Instalaciones)
+# * Res:  Promedio = 22.77 |  Mediana = 14 
+# ¿Cuánto cuesta la aplicación más cara? 
+# *Res: 400
+
+# (Extra) Especifique la media aritmética y la mediana del número de instalaciones de aplicaciones (Instalaciones)
+# * Res:  Promedio = 8662313 |  Mediana = 100000 
+print(df.describe())
--- analisis_de_datos/01-fundamentos_y_filtrado/ejercicios/ruta-2/your_code_2.py
+++ analisis_de_datos/01-fundamentos_y_filtrado/ejercicios/ruta-2/your_code_2.py
@@ -2,21 +2,34 @@ import pandas as pd
 df = pd.read_csv ('GoogleApps.csv')
 
 # ¿Cuál es el precio (Price) de la aplicación de pago más barata (Type == 'Paid')?
+# * Res: 0.99
+print(df[df['Type'] == 'Paid']['Price'].min())
 
 
 # Cuál es la mediana (median) del número de instalaciones (Installs)
 # de aplicaciones de la categoría "ART_AND_DESIGN" (Category)?
-
+# * Res: 100000.0
+print(df[df['Category'] == 'ART_AND_DESIGN']['Installs'].median())
 
 # ¿Por cuánto el número máximo de reseñas para las aplicaciones gratuitas (Type == 'Free')
 # rebasa el número máximo de reseñas para las aplicaciones de pago (Type == 'Paid')?
+# * Res: 44703802
 
+free = df[df['Type'] == 'Free']['Reviews'].max()
+paid = df[df['Type'] == 'Paid']['Reviews'].max()
+print(free - paid)
 
 # ¿Cuál es el tamaño mínimo (Size) de una aplicación para adolescentes (Content Rating == 'Teen')?
+# * Res: 0.315
+print(df[df['Content Rating'] == 'Teen']['Size'].min())
 
 
 # (Extra) ¿Cuál es la categoría (Category) de una aplicación con el mayor número de reseñas (Reviews)?
+# * Res: GAME
+print(df[df['Reviews'] == df['Reviews'].max()]['Category'])
 
 
 # (Extra) ¿Cuál es la valoración (Rating) media (mean) de las aplicaciones con un precio (Price) superior a $ 20 y 
 # con el número de instalaciones (Installs) más de 10.000?
+# * Res: 4.25
+print(df[(df['Price'] > 20) & (df['Installs'] > 10000)]['Rating'].mean())
