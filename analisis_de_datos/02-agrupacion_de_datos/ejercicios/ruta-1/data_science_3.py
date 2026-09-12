a38df3bcc6c8938e67e280e85532c523a769b451
dalaiboada <dalaiboadajob@gmail.com>
2026-09-12 09:46:39 -0400

    feat(analisis de datos): agrupacion de datos

--- analisis_de_datos/02-agrupacion_de_datos/ejercicios/ruta-1/data_science_3.py
+++ analisis_de_datos/02-agrupacion_de_datos/ejercicios/ruta-1/data_science_3.py
@@ -1,22 +1,66 @@
 import pandas as pd
 df = pd.read_csv('GoogleApps.csv')
 
-# 1 ¿Cuántas aplicaciones hay en la 'Category' 'BUSINESS' 'Category'?
+print('\nDatos:')
+print(df)
 
-# 2 ¿Cuál es la relación de aplicaciones para adolescentes ('Teen') y las destinadas para niños mayores de 10 años ('Everyone 10+')?
+print('\nInfo:')
+print(df.info())
+
+
+#1 ¿Cuántas aplicaciones hay en la 'Category' 'BUSINESS'?
+# * Res. 246
+print('\nCantidad de aplicaciones por cateoría:')
+print(df['Category'].value_counts())
+
+
+#2 ¿Cuál es la relación (división) de aplicaciones para adolescentes ('Teen') 
+# y las destinadas para niños mayores de 10 años ('Everyone 10+')?
 # Redondee la respuesta a la centésima más cercana.
-
-# 3.1 ¿Cuál es el 'Rating' promedio de aplicaciones 'Paid'? 
-# Redondee la respuesta a la centésima más cercana.
-
-# 3.2 ¿Cuánto más bajo es el 'Rating' promedio de aplicaciones 'Free' que el promedio de valoración de las aplicaciones 'Paid'?
-# Redondee la respuesta a la centésima más cercana.
-
-# 4 ¿Cuál es el 'Size' (tamaño) mínimo y máximo en la 'Category' 'COMICS'?
-# Redondee la respuesta a la centésima más cercana.
-
-# Bonificación 1. ¿Cuántas aplicaciones tienen un 'Rating' estrictamente superior a 4.5 en la 'Category' 'FINANCE'?
-
-# Bonificación 2. ¿Cuál es la relación de juegos 'Free' y 'Paid' con un 'Rating' superior a 4.9?
+# * Res 2.73
+print('\nRelación entre las aplicaciones para adolescentes y las de mayores de 10:')
+temp = df['Content Rating'].value_counts()
+print('Ratio:', round(temp['Teen'] / temp['Everyone 10+'], 2))
 
 
+#3.1 ¿Cuál es el 'Rating' promedio de aplicaciones 'Paid'?
+#Redondee la respuesta a la centésima más cercana.
+# * Res 4.25
+
+print('\nRating promedio de aplicaciones pagas(Paid)')
+temp = df.groupby('Type')['Rating'].mean()
+print(temp['Paid'])
+
+
+#3.2 ¿Cuánto más bajo (restar) es el 'Rating' promedio de aplicaciones 'Free' 
+# que el promedio de valoración de las aplicaciones 'Paid'?
+
+#Redondee la respuesta a la centésima más cercana.
+
+# * Res 0.08
+
+print('\nDiferencia entre rating promedio de aplicaciones pagas y gratis:')
+print(round(temp['Paid'] - temp['Free'], 2))
+
+
+#4 ¿Cuál es el 'Size' (tamaño) mínimo y máximo en la 'Category' 'COMICS'?
+#Redondee la respuesta a la centésima más cercana.
+
+# * Res min: 0.43  max 40.0
+
+print('\nSize minimo y maximo para la categoria COMICS:')
+print(df.groupby('Category')['Size'].agg(['min', 'max']))
+
+
+#Bonificación 1. ¿Cuántas aplicaciones tienen un 'Rating' de más de 4.5 en la 'Category' 'FINANCE'?
+print('\nBonificación 1:')
+
+temp = df[df['Rating'] > 4.5]['Category'].value_counts()
+print(temp['FINANCE'])
+
+
+#Bonificación 2. ¿Cuál es la relación de juegos 'Free' y 'Paid' con un 'Rating' superior a 4.9?
+print('\nBonificación 2:')
+
+temp = df[(df['Category'] == 'GAME') & (df['Rating'] > 4.9)]['Type'].value_counts()
+print(temp['Free'] / temp['Paid'])
--- analisis_de_datos/02-agrupacion_de_datos/ejercicios/ruta-2/data_science_4.py
+++ analisis_de_datos/02-agrupacion_de_datos/ejercicios/ruta-2/data_science_4.py
@@ -1,25 +1,57 @@
 import pandas as pd
 df = pd.read_csv('GoogleApps.csv')
 
-# 1 Muestre el 'Rating' mínimo, promedio, y máximo de aplicaciones de pago y gratuitas ('Type') y redondee a la décima más cercana.
 
-# 2 Muestre el 'Price' mínimo, medio, y máximo de aplicaciones de pago (Type == 'Paid') para 
-# diferentes públicos objetivo ('Content Rating')
+#1 Muestre el 'Rating' mínimo, promedio, y máximo de aplicaciones de pago y gratuitas ('Type') y redondee a la décima más cercana.
+print('\nDatos:')
+print(round(df.groupby('Type')['Rating'].agg(['min', 'mean', 'max']), 1))
 
-# 3 Agrupe los datos por 'Category' y público objetivo ('Content Rating') como prefiera
-# calcule el número máximo de 'Reviews' para cada grupo.
-# Comparte los resultados para las categorías 'EDUCATION', 'FAMILY, y 'GAME'.
-# ¿En qué grupo de edad recibió más reseñas la aplicación en la categoría 'EDUCATION'? ¿'FAMILY'? ¿'GAME'?
-# Consejo: Puede seleccionar múltiples columnas del DataFrame a la vez usando la siguiente sintaxis:
+
+#2 Muestre el 'Price' mínimo, medio, y máximo de aplicaciones de pago (Type == 'Paid') para # diferentes públicos objetivo ('Content Rating')
+print('\nDatos:')
+print(df[df['Type'] == 'Paid'].groupby('Content Rating')['Price'].agg(['min', 'median', 'max']))
+
+
+#3 Agrupar los datos por 'Category' y público objetivo ('Content Rating') como prefiera
+#calcule el número máximo de 'Reviews' para cada grupo.
+#Compare los resultados para las categorías 'EDUCATION', 'FAMILY, y 'GAME'.
+#¿En qué grupo de edad recibió más reseñas la aplicación en la categoría 'EDUCATION'? ¿'FAMILY'? ¿'GAME'?
+
+
+#Consejo: Puede seleccionar múltiples columnas del DataFrame a la vez usando la siguiente sintaxis:
 # df[[<column 1>, <column 2>, <column 3>]]
 
+print('\nDatos:')
 
-# 4 Agrupe las aplicaciones de pago (Type == 'Paid') por 'Category' y por público objetivo ('Content Rating')
-# Calcule el número promedio de 'Reviews' para cada grupo
-# Tenga en cuenta que algunas celdas en la tabla de resultados tienen el valor "NaN" – Not A Number – en lugar de un número
-# Eso significa que no hay aplicaciones en ese grupo.
-# Elija los nombres de las categorías que tengan aplicaciones de pago para todos los grupos de edad y ordénelas por orden alfabético.
+temp = df.pivot_table(
+  index = 'Content Rating', 
+  columns = 'Category', 
+  values = 'Reviews', 
+  aggfunc = 'max'
+)
+print(temp[['EDUCATION', 'FAMILY', 'GAME']])
 
-# Tarea de bonificación Encuentre las categorías de aplicaciones gratuitas (Type == 'Free') 
-# en las cuales las aplicaciones no estaban diseñadas para todos los grupos de edad ('Content Rating')
 
+#4 Agrupe las aplicaciones de pago (Type == 'Paid') por 'Category' y por público objetivo ('Content Rating')
+#Calcule el número promedio de 'Reviews' para cada grupo
+#Tenga en cuenta que algunas celdas en la tabla de resultados tienen el valor "NaN" – Not A Number – en lugar de un número
+#Eso significa que no hay aplicaciones en ese grupo.
+#Elija los nombres de las categorías que tengan aplicaciones de pago para todos los grupos de edad y ordénelas por orden alfabético.
+
+print('\nDatos:')
+print(df[df['Type'] == 'Paid'].pivot_table(
+  columns = 'Content Rating', 
+  index = 'Category', 
+  values = 'Reviews', 
+  aggfunc = 'mean'
+))
+
+
+#Tarea de bonificación. Encuentre las categorías de aplicaciones gratuitas (Type == 'Free')
+#en qué grupos las aplicaciones no estaban diseñadas para todos los grupos de edad ('Content Rating')
+print(df[df['Type'] == 'Free'].pivot_table(
+  index = 'Category', 
+  columns = 'Content Rating', 
+  values = 'Reviews', 
+  aggfunc = 'mean'
+))
